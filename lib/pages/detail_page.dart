8bd702f136d29a4c57fd0871d38d5ec0f817bbab
MOH ROFIKI <fikimlro95@gmail.com>
2026-05-17 16:40:22 +0700

    Update Data di Detail page

--- lib/pages/detail_page.dart
+++ lib/pages/detail_page.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
+import 'dart:io';
+import 'package:model_viewer_plus/model_viewer_plus.dart';
+import 'package:dio/dio.dart';
+import 'package:path_provider/path_provider.dart';
+import '../models/asset_model.dart';
 
-class DetailPage extends StatelessWidget {
+class DetailPage extends StatefulWidget {
   const DetailPage({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('Detail')),
-      body: const Center(child: Text('Detail Page', style: TextStyle(color: Colors.white))),
-    );
-  }
+  State<DetailPage> createState() => _DetailPageState();
 }
+
+
