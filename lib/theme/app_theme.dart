92bfc65a5852d0f9e26f79c30665a5664dc38de4
MOH ROFIKI <fikimlro95@gmail.com>
2026-05-23 00:45:44 +0700

    Manajemen Status Global & Pembaruan Root

--- lib/theme/app_theme.dart
+++ lib/theme/app_theme.dart
@@ -1,6 +1,22 @@
 import 'package:flutter/material.dart';
 
 class AppTheme {
+  static ThemeData get lightTheme {
+    return ThemeData.light().copyWith(
+      scaffoldBackgroundColor: Colors.white,
+      appBarTheme: const AppBarTheme(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        iconTheme: IconThemeData(color: Colors.black),
+      ),
+      textTheme: const TextTheme(
+        bodyLarge: TextStyle(color: Colors.black),
+        bodyMedium: TextStyle(color: Colors.black),
+      ),
+      iconTheme: const IconThemeData(color: Colors.black),
+    );
+  }
+
   static ThemeData get darkTheme {
     return ThemeData.dark().copyWith(
       scaffoldBackgroundColor: Colors.black,
@@ -13,6 +29,7 @@ class AppTheme {
         bodyLarge: TextStyle(color: Colors.white),
         bodyMedium: TextStyle(color: Colors.white),
       ),
+      iconTheme: const IconThemeData(color: Colors.white),
     );
   }
 }
